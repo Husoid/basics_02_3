98a205f0f12810d3855c710cdf8c5486ed1fd8f3
Husoid <husoid@mail.ru>
2023-06-15 11:20:09 +0500

    work error

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,16 @@
 fun main() {
     var isRegularCustomer = true
     var pay = 100
-    var finalPay = pay - pay * 5 / 100
-    if (isRegularCustomer) {
-        finalPay -= if ((finalPay * 1 / 100)<=0) {1} else {finalPay * 1 / 100}
-    }
+    var finalPay =
+        if (isRegularCustomer) {
+            if (((pay - pay * 5 / 100) * 1 / 100) <= 0) {
+                (pay - pay * 5 / 100) - 1
+            } else {
+                (pay - pay * 5 / 100) * 1 / 100
+            }
+        } else {
+            pay - pay * 5 / 100
+        }
 
     println("покупка на $pay рублей будет со скидной и обойдется Вам в $finalPay рублей")
 }
