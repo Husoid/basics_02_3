697bbeeee799444dde22cfc0b3377f2d7fbabd60
Husoid <husoid@mail.ru>
2023-07-07 21:47:53 +0500

    поправил замечание

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,30 @@
+
 fun main() {
-    var isRegularCustomer = true
-    var pay = 100
-    var finalPay =
+    val isRegularCustomer = true
+    var pay = 900
+    val finalPay = if (pay <= 1000) {
         if (isRegularCustomer) {
-            if (((pay - pay * 5 / 100) * 1 / 100) <= 0) {
-                (pay - pay * 5 / 100) - 1
+            if ((pay * 1 / 100) <= 0) {
+                pay - 1
             } else {
-                (pay - pay * 5 / 100) * 1 / 100
+                pay - pay * 1 / 100
             }
+        } else {
+            pay
+        }
+    } else if ((pay > 1000)&&(pay <= 10000)) {
+        if (isRegularCustomer) {
+            (pay - 100) * 1 / 100
+        } else {
+            pay -= 100
+        }
+    } else {
+        if (isRegularCustomer) {
+            pay - pay * 5 / 100 * 1 / 100
         } else {
             pay - pay * 5 / 100
         }
+    }
 
     println("покупка на $pay рублей будет со скидной и обойдется Вам в $finalPay рублей")
 }
